e42c290f59cf8a08e1bab7352bc6f50d1d633c75
Piotr Buczek <piotr.buczek37@gmail.com>
2020-04-21 09:15:41 +0200

    Added CourierActivity with packages management

---
+++ app/src/main/java/com/ugprojects/couriertracerdpd/layoutElements/PackagesListAdapter.java
@@ -0,0 +1,77 @@
+package com.ugprojects.couriertracerdpd.layoutElements;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ugprojects.couriertracerdpd.R;
+import com.ugprojects.couriertracerdpd.model.Package;
+
+import java.util.List;
+
+public class PackagesListAdapter extends RecyclerView.Adapter<PackagesListAdapter.ViewHolder> {
+
+    private List<Package> packageList;
+    private Context context;
+
+    public PackagesListAdapter(List<Package> packageList, Context context) {
+        this.packageList = packageList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.courier_list_item,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder holder, final int position) {
+        Package pack = packageList.get(position);
+
+        holder.packageNumber.setText(pack.getPackageNumber());
+        String packageAddressText = pack.getAddress()+", "+pack.getPostCode();
+        holder.packageAddress.setText(packageAddressText);
+
+        holder.deleteItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                packageList.remove(position);
+                notifyDataSetChanged();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return packageList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView packageNumber;
+        public TextView packageAddress;
+        public LinearLayout linearLayout;
+        public ImageView deleteItem;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            packageNumber = itemView.findViewById(R.id.packageNumberTitle);
+            packageAddress = itemView.findViewById(R.id.packageAddress);
+            linearLayout = itemView.findViewById(R.id.packageListLinearLayout);
+            deleteItem = itemView.findViewById(R.id.deleteItem);
+        }
+
+    }
+}
